956a722064fb466f3317e9c91a0a21a56ae21cd9
bigmike0009 <mikea0009@gmail.com>
2021-01-15 14:26:26 -0500

    Karen is so much hotter than Pam

--- algorithm-api/app.py
+++ algorithm-api/app.py
@@ -11,6 +11,13 @@ app = Flask(__name__)
 def form():
     return "Use /gap endpoint to launch gap down algorithm with mikes money and /cash to see his balance(checking)"
 
+@app.route("/open/<ticker>")
+def open(ticker):
+    #print(request.args) url parameters in the format [('interval', '1D'), ('num_bars', '1')]
+    #print(request.form) request body [('body', 'by a singing hitta')]
+    #print(request.headers) HTTP headers as string? it seems
+    return str(MarketDataFunctions.get_open(ticker, "1D", 1))
+
 
 @app.route("/gap")
 def my_form_post():
