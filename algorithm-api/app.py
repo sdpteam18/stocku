8478f94b5f0f23f7345abcf0d8169fcd960d497e
Ben Levy <benlevy@Bens-MacBook-Pro.local>
2021-01-15 21:01:00 -0500

    first mongo test

--- algorithm-api/app.py
+++ algorithm-api/app.py
@@ -1,10 +1,26 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask import render_template, request
+from flask_cors import CORS
+import pymongo
+import urllib.parse
+
 from Gap import *
 from beta_functions import *
+
+username = urllib.parse.quote_plus('Ben')
+password = urllib.parse.quote_plus('MongoPass12')
+
+connection_url = 'mongodb+srv://{}:{}@cluster0.xliu9.mongodb.net/test_database?retryWrites=true&w=majority'.format(
+    username, password)
+
 app = Flask(__name__)
+client = pymongo.MongoClient(connection_url)
+
+Database = client.get_database('test_database')
+
+SampleTable = Database.test_collection
 
 
 @app.route("/")
@@ -25,6 +41,18 @@ def parallels():
 @app.route("/open")
 def aapl_open():
     return get_open('AAPL', '1D', 1)
+
+
+@app.route('/find/', methods=['GET'])
+def findAll():
+    query = SampleTable.find()
+    output = {}
+    i = 0
+    for x in query:
+        output[i] = x
+        output[i].pop('_id')
+        i += 1
+    return jsonify(output)
 
 
 if __name__ == "__main__":
